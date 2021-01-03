a8f80aeb810601cf9038714942a662d0d15e99c5
github Ryuya-web <mofa196@mbox.re>
2021-01-04 00:01:05 +0900

    Revert "Revert "Update firbase""
    This reverts commit 0038e405da7891e70f0e047b56f0472e2e4eb1d8.

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCUusz1zOmJCXQLuZNrCqPUYwHP7qe2Q_g",
+    authDomain: "chatbot-demo2-ca932.firebaseapp.com",
+    projectId: "chatbot-demo2-ca932",
+    storageBucket: "chatbot-demo2-ca932.appspot.com",
+    messagingSenderId: "927442957439",
+    appId: "1:927442957439:web:3ee79fa1dc27f34e4a7b4a",
+    measurementId: "G-Y5THTDCKH0"
+  };
+
+export default firebaseConfig
